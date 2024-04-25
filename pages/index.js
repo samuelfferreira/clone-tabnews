4022f0ef916c8e74e4fd499d9f2722f9abe40d59
Samuel Ferreira <samuel_ferreira@outlook.com>
2024-04-25 07:41:56 -0300

    inclusao de prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Crie o hábio de se perguntar todos os dias:<br></br> "isso me afasta ou me aproxima dos meus objetivos?"</h1>
+  return (
+    <h1>
+      Crie o hábio de se perguntar todos os dias:<br></br> "isso me afasta ou me
+      aproxima dos meus objetivos?"
+    </h1>
+  );
 }
 
 export default Home;
