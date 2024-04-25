c863bb671d90d634547216155fa02f0d6704c6cb
Samuel Ferreira <samuel_ferreira@outlook.com>
2024-04-25 07:25:03 -0300

    inclusao de arquivo de editor config

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Crie o hábio de se perguntar todos os dias:<br></br> "isso me afasta ou me aproxima dos meus objetivos?"</h1>
+  return <h1>Crie o hábio de se perguntar todos os dias:<br></br> "isso me afasta ou me aproxima dos meus objetivos?"</h1>
 }
 
 export default Home;
