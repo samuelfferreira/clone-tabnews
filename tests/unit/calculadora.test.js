36ee16ee2862038d2e0f82210f559883867b7b20
Samuel Ferreira <samuel_ferreira@outlook.com>
2024-04-26 16:37:19 -0300

    incluido endpoint de status

---
+++ tests/unit/calculadora.test.js
@@ -0,0 +1,12 @@
+const calculadora = require("../../models/calculadora");
+
+test("soma 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+
+  expect(resultado).toBe(4);
+});
+
+test("soma 5 + 100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
