8b060b8e4f4fdf62b83d085ab36d12ab964cd024
Samuel Ferreira <samuel_ferreira@outlook.com>
2024-04-27 16:15:28 -0300

    inclusao de conexao com banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,40 @@
-import database from "../../../../infra/database";
+import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1;");
-  console.log(result);
+  const databaseVersionResult = await database.query("show server_version;");
+  const updatedAt = new Date().toISOString();
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "show max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  //vulneravel
+  // const databaseOpenedConnectionsResult = await database.query(
+  //   `select count(*)::int from pg_stat_activity where datname = '${databaseName}';`,
+  // );
+
+  //nao vulneravel
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "select count(*)::int from pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    chave: "valor",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,29 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3001/api/v1/status");
 
+  var responseBody = await response.json();
+  //console.log(responseBody);
+
   expect(response.status).toBe(200);
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parseUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toEqual("16.2");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
+
+// test.only("Teste sql injection", async () => {
+//   // const response = await fetch(
+//   //   "http://localhost:3001/api/v1/status?databaseName=local_db",
+//   // );
+
+//   const response = await fetch(
+//     "http://localhost:3001/api/v1/status?databaseName='; select pg_sleep(4); --",
+//   );
+// });
