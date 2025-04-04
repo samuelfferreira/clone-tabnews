103c1f192590a66b7c88ce9fb6b27ecabbabab00
Samuel Ferreira <samuel_ferreira@outlook.com>
2025-04-04 17:09:19 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Crie o hábio de se perguntar todos os dias:<br></br> "isso me afasta ou me
-      aproxima dos meus objetivos??"
+      aproxima dos meus objetivos?"
     </h1>
   );
 }
