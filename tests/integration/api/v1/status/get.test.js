ca6dff22c3565b50f055a50380c4a304a578cabb
Samuel Ferreira <samuel_ferreira@outlook.com>
2025-04-12 13:35:07 -0300

    build: lock dependencies versions

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,7 +5,7 @@ beforeAll(async () => {
 });
 
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  const response = await fetch("http://localhost:3001/api/v1/status");
 
   let responseBody = await response.json();
 
