29905b964ddaf4823308a2ba3052f8b8347d3383
Samuel Ferreira <samuel_ferreira@outlook.com>
2025-04-13 06:08:01 -0300

    ci: add husky and commit-msg book

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
 
 describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    test("Running pending migrations", async () => {
+    test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations");
       let responseBody = await response.json();
       expect(Array.isArray(responseBody)).toBe(true);
